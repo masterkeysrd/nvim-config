68def7de7ddf902a18bbd42b1a889ea80bbef59f
David Morales <morales.david1997@gmail.com>
2024-04-12 18:14:31 -0400

    chore: add split keymaping

--- lua/masterkeysrd/remap.lua
+++ lua/masterkeysrd/remap.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("i", "jj", "<Esc>")
+
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
