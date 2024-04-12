ae66a34ce8ef01e832c906a7091b99ea639d0446
David Morales <morales.david1997@gmail.com>
2024-04-12 17:48:52 -0400

    chore(set): set tabbing config

--- lua/masterkeysrd/set.lua
+++ lua/masterkeysrd/set.lua
@@ -2,3 +2,13 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- indent
+vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- scrolloff
+vim.opt.scrolloff = 10
+
+
