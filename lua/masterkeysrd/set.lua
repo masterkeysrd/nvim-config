307095b31ed7b714b0950b126b7f22d6d3edde12
David Morales <morales.david1997@gmail.com>
2024-04-15 21:15:24 -0400

    chore(set): update editor configuration

--- lua/masterkeysrd/set.lua
+++ lua/masterkeysrd/set.lua
@@ -1,19 +1,36 @@
+vim.opt.guicursor = ""
+
 -- set line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- indent
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 vim.opt.smartindent = true
-vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.opt.tabstop = 2 -- A TAB character looks like 4 spaces
-vim.opt.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
-vim.opt.shiftwidth = 2 -- Number of spaces inserted when indenting
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
 
 -- clipboard
 vim.opt.clipboard = 'unnamedplus'
 
 -- scrolloff
 vim.opt.scrolloff = 10
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
 
 -- faster completion
 vim.opt.updatetime = 250
@@ -27,4 +44,4 @@ vim.g.loaded_netrwPlugin = 1
 
 -- editor configs
 vim.opt.colorcolumn = "80"
-
+vim.opt.updatetime = 50
