9ddc94cd6634adddde55221305e6aab8d77578bd
David Morales <morales.david1997@gmail.com>
2024-04-18 10:51:34 -0400

    chore: add support for dotfiles

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,37 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+telescope.setup({
+    defaults = {
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--trim",
+            '--hidden',
+            '--no-ignore',
+            '--glob',
+            '!**/.git/*'
+        },
+    },
+    pickers = {
+        find_files = {
+            find_command = {
+                'rg',
+                '--files',
+                '--hidden',
+                '--no-ignore',
+                '--glob',
+                '!**/.git/*'
+            }
+        }
+    }
+})
+
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 
--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,18 +1,22 @@
-vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
-vim.keymap.set("n", "<leader>r", vim.cmd.NvimTreeRefresh)
-vim.keymap.set("n", "<leader>n", vim.cmd.NvimTreeFindFile)
-
 require("nvim-tree").setup({
   sort = {
     sorter = "case_sensitive",
   },
   view = {
-    width = 30,
+    width = 35,
   },
   renderer = {
     group_empty = true,
   },
   filters = {
-    dotfiles = true,
+    dotfiles = false,
+    git_clean = false,
+    no_buffer = false,
+    custom = { '.git' }
   },
 })
+
+vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>r", vim.cmd.NvimTreeRefresh)
+vim.keymap.set("n", "<leader>n", vim.cmd.NvimTreeFindFile)
+
