2e267d4e39e0d55b9ec913842ca7940a06b6324b
David Morales <morales.david1997@gmail.com>
2024-04-18 12:32:46 -0400

    chore(explorer): update to show ignore files and dotfiles

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,4 +1,5 @@
 require("nvim-tree").setup({
+  disable_netrw = false,
   sort = {
     sorter = "case_sensitive",
   },
@@ -7,16 +8,54 @@ require("nvim-tree").setup({
   },
   renderer = {
     group_empty = true,
+    highlight_git = true,
+    icons = {
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        },
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+        },
+      },
+    },
   },
   filters = {
-    dotfiles = false,
-    git_clean = false,
-    no_buffer = false,
-    custom = { '.git' }
+    git_ignored = false,
+    custom = { '^.git$' }
   },
+  diagnostics = {
+    enable = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
+  update_focused_file = {
+    enable = true,
+  }
 })
 
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<leader>r", vim.cmd.NvimTreeRefresh)
 vim.keymap.set("n", "<leader>n", vim.cmd.NvimTreeFindFile)
-
