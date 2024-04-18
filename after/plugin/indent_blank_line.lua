40d483d059a4d7d17439a4d4cb195923fcf69bcd
David Morales <morales.david1997@gmail.com>
2024-04-18 12:49:07 -0400

    chore: add indent blank line

---
+++ after/plugin/indent_blank_line.lua
@@ -0,0 +1,24 @@
+require('ibl').setup({
+  indent = {
+    char = "│",
+    tab_char = "│",
+  },
+  scope = {
+    enabled = false
+  },
+  exclude = {
+    filetypes = {
+      "help",
+      "alpha",
+      "dashboard",
+      "neo-tree",
+      "Trouble",
+      "trouble",
+      "lazy",
+      "mason",
+      "notify",
+      "toggleterm",
+      "lazyterm",
+    },
+  },
+})
--- lua/masterkeysrd/packer.lua
+++ lua/masterkeysrd/packer.lua
@@ -4,46 +4,47 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use { 'wbthomason/packer.nvim' }
-    use {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.6',
-        requires = 'nvim-lua/plenary.nvim'
+  -- Packer can manage itself
+  use { 'wbthomason/packer.nvim' }
+  use {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.6',
+    requires = 'nvim-lua/plenary.nvim'
+  }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    { run = ':TSUpdate' }
+  }
+  use { 'tpope/vim-fugitive' }
+  use { 'nvim-lualine/lualine.nvim' }
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+      { 'neovim/nvim-lspconfig' },
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
     }
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        { run = ':TSUpdate' }
-    }
-    use { 'tpope/vim-fugitive' }
-    use { 'nvim-lualine/lualine.nvim' }
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        requires = {
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-            { 'neovim/nvim-lspconfig' },
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
-        }
-    }
-    use {
-        'akinsho/toggleterm.nvim',
-        config = true
-    }
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = 'nvim-tree/nvim-web-devicons'
-    }
-    use {
-        'akinsho/bufferline.nvim',
-        tag = "*",
-        requires = 'nvim-tree/nvim-web-devicons'
-    }
-    use { 'RRethy/vim-illuminate' }
-    use { 'stevearc/conform.nvim' }
-    use { 'mfussenegger/nvim-lint' }
-    use { 'rose-pine/neovim' }
+  }
+  use {
+    'akinsho/toggleterm.nvim',
+    config = true
+  }
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = 'nvim-tree/nvim-web-devicons'
+  }
+  use {
+    'akinsho/bufferline.nvim',
+    tag = "*",
+    requires = 'nvim-tree/nvim-web-devicons'
+  }
+  use { 'RRethy/vim-illuminate' }
+  use { 'stevearc/conform.nvim' }
+  use { 'mfussenegger/nvim-lint' }
+  use { 'lukas-reineke/indent-blankline.nvim' }
+  use { 'rose-pine/neovim' }
 end)
