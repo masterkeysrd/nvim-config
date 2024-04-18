67f8ba142261ace58803db20803e38f425bae72c
David Morales <morales.david1997@gmail.com>
2024-04-18 12:38:44 -0400

    style(bfline): add identation

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,16 +1,15 @@
 require('bufferline').setup({
-    options = {
-        diagnostics = "nvim_lsp",
-        always_show_bufferline = false,
-        offsets = {
-            {
-                filetype = "NvimTree",
-                text="Explorer",
-                separator= true,
-                highlight = "Directory",
-                text_align = "left",
-            },
-        },
+  options = {
+    diagnostics = "nvim_lsp",
+    always_show_bufferline = false,
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "Explorer",
+        separator = true,
+        highlight = "Directory",
+        text_align = "left",
+      },
     },
+  },
 })
-
