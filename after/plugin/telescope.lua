cc53489a051016bea7ca5caed911e365e0d8c068
David Morales <morales.david1997@gmail.com>
2024-04-18 22:56:45 -0400

    chore: update to skip packages directories

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,36 +2,41 @@ local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
 telescope.setup({
-    defaults = {
-        vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-            "--trim",
-            '--hidden',
-            '--no-ignore',
-            '--glob',
-            '!**/.git/*'
-        },
+  defaults = {
+    file_ignore_patterns = {
+      ".git",
+      ".yarn",
+      "node_modules",
+      "vendor"
     },
-    pickers = {
-        find_files = {
-            find_command = {
-                'rg',
-                '--files',
-                '--hidden',
-                '--no-ignore',
-                '--glob',
-                '!**/.git/*'
-            }
-        }
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--trim",
+      '--hidden',
+      '--no-ignore',
+      '--glob',
+      '!**/.git/*'
+    },
+  },
+  pickers = {
+    find_files = {
+      find_command = {
+        'rg',
+        '--files',
+        '--hidden',
+        '--no-ignore',
+        '--glob',
+        '!**/.git/*'
+      }
     }
+  }
 })
 
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-
