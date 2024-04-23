4f3de52bde38ab27384ad134f4b475e52e979444
David Morales <morales.david1997@gmail.com>
2024-04-23 10:28:24 -0400

    chore(comp): update to remove tab key

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -35,7 +35,6 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-e>'] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
