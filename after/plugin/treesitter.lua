f104829fecc04f8d98336b2ad2cc73cb01271159
David Morales <morales.david1997@gmail.com>
2024-05-02 16:20:15 -0400

    chore: set up treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,12 +1,9 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "javascript", "typescript", "python", "json" },
+require 'nvim-treesitter.configs'.setup {
   sync_install = false,
   auto_install = true,
-  highlight = {
-    enable = true,
-  },
+  highlight = { enable = true },
+  indent = { enable = true },
 }
 
 vim.treesitter.language.register('xml', { 'svg', 'xslt' })
 vim.treesitter.language.register('json', { 'json.tmpl' })
-
