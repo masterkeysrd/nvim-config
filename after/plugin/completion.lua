9c5d864a0936725976f10fd4718a6ef71af27d9d
David Morales <morales.david1997@gmail.com>
2024-05-02 16:18:33 -0400

    chore: setup completion

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -38,7 +38,8 @@ cmp.setup({
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'path' }
+    { name = 'path' },
+    { name = 'luasnip' },
   }, { name = 'buffer' }),
   formatting = {
     fields = { 'kind', 'abbr' },
