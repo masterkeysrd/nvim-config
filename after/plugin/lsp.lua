b79cfd8e9ac8f8dd4f9b36c5c84b1d76a560539a
David Morales <morales.david1997@gmail.com>
2024-05-02 21:58:32 -0400

    chore: setup lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,32 +2,17 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommend')
 
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+lsp.on_attach(function(_, bufnr)
+  lsp.default_keymaps({ buffer = bufnr })
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {
-	'ast_grep',
-	'bashls',
-	'clangd',
-	'cssls',
-	'dockerls',
-	'docker_compose_language_service',
-	'eslint',
-	'gopls',
-	'tsserver',
-	'html',
-	'lua_ls',
-	'marksman',
-	'spectral',
-	'pyright',
-	'sqlls',
-	'yamlls',
-  },
+  ensure_installed = {},
   automatic_installation = true,
   handlers = {
-    lsp.default_setup,
-  },
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end
+  }
 })
