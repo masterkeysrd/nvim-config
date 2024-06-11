55be5f11f28b22ab23f76b7d59a35dd326304637
David Morales <morales.david1997@gmail.com>
2024-06-11 08:57:37 -0400

    feat(trouble): add todo trouble and indent code

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,7 +1,23 @@
 local trouble = require('trouble')
 
 trouble.setup({})
-vim.keymap.set("n", "<leader>xx", function() trouble.toggle('diagnostics') end)
-vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end)
-vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end)
-vim.keymap.set("n", "gR", function() trouble.toggle("lsp_references") end)
+
+vim.keymap.set("n", "<leader>xx", function()
+  trouble.toggle('diagnostics')
+end, { silent = true, desc = 'Trouble diagnostics' })
+
+vim.keymap.set("n", "<leader>xq", function()
+  trouble.toggle("quickfix")
+end, { silent = true, desc = 'Trouble quickfix' })
+
+vim.keymap.set("n", "<leader>xl", function()
+  trouble.toggle("loclist")
+end, { silent = true, desc = 'Trouble loclist' })
+
+vim.keymap.set("n", "gR", function()
+  trouble.toggle("lsp_references")
+end, { silent = true, desc = 'Trouble lsp_references' })
+
+vim.keymap.set("n", "<leader>xt", function()
+  trouble.open('todo')
+end, { silent = true, desc = 'Trouble todo' })
