fadbdec9534de13aff928bbe80f8f5acfaab272b
David Morales <morales.david1997@gmail.com>
2024-06-10 23:26:33 -0400

    feat(color): update theme config

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -4,10 +4,49 @@ function ColorMyPencils(color)
 end
 
 require('rose-pine').setup({
+  variant = 'moon',
+  dark_variant = 'moon',
+  dim_inactive_windows = false,
+  extend_background_behind_borders = true,
+
   styles = {
+    bold = true,
     italic = false,
+    transparency = false,
   },
+
+
+  groups = {
+    border = "muted",
+    link = "iris",
+    panel = "surface",
+
+    error = "love",
+    hint = "iris",
+    info = "foam",
+    note = "pine",
+    todo = "rose",
+    warn = "gold",
+
+    git_add = "foam",
+    git_change = "rose",
+    git_delete = "love",
+    git_dirty = "rose",
+    git_ignore = "muted",
+    git_merge = "iris",
+    git_rename = "pine",
+    git_stage = "iris",
+    git_text = "rose",
+    git_untracked = "subtle",
+
+    h1 = "iris",
+    h2 = "foam",
+    h3 = "rose",
+    h4 = "gold",
+    h5 = "pine",
+    h6 = "foam",
+  }
 })
 
-vim.cmd("colorscheme rose-pine-moon")
+vim.cmd("colorscheme rose-pine")
 ColorMyPencils()
