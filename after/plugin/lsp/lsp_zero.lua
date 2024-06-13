9d3f18e1ef99db483c080937ccb9e0a70a8f04da
David Morales <morales.david1997@gmail.com>
2024-06-13 18:23:24 -0400

    feat(lsp): add keymaps

--- after/plugin/lsp/lsp_zero.lua
+++ after/plugin/lsp/lsp_zero.lua
@@ -6,13 +6,34 @@ if not ok then
   return
 end
 
-lsp_zero.preset('recommended')
-
 lsp_zero.on_attach(function(_, bufnr)
   lsp_zero.default_keymaps({
     buffer = bufnr,
     preserve_mappings = false,
   })
+
+  local keymap = vim.keymap.set
+
+  local function opts(desc)
+    return { noremap = true, silent = true, desc = desc }
+  end
+
+  -- LSP
+  keymap("n", "<leader>cl", "<cmd>LspInfo<cr>", opts("Show LSP info"))
+  keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts("Show code actions"))
+  keymap("n", "<leader>cr", vim.lsp.buf.rename, opts("Rename symbol"))
+
+  if vim.lsp.inlay_hint then
+    keymap("n", "<leader>ch", function()
+      local is_enabled = vim.lsp.inlay_hint.is_enabled
+      vim.lsp.inlay_hint.enable(not is_enabled())
+    end, opts("Show inlay hints"))
+  end
+
+  if vim.lsp.codelens then
+    keymap("n", "<leader>cc", vim.lsp.codelens.run, opts("Show code lens"))
+    keymap("n", "<leader>cC", vim.lsp.codelens.refresh, opts("Show code lens"))
+  end
 end)
 
 lsp_zero.set_sign_icons({
