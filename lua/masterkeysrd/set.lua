5a34c4c22623a610e8151624bde61a207be88a48
David Morales <morales.david1997@gmail.com>
2024-06-12 23:02:18 -0400

    docs(set): add documentation

--- lua/masterkeysrd/set.lua
+++ lua/masterkeysrd/set.lua
@@ -1,3 +1,4 @@
+-- set global options
 vim.opt.guicursor = ""
 
 -- set line numbers
@@ -10,19 +11,22 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
+-- line wrap
 vim.opt.wrap = false
 
+-- undo/redo
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- terminal colors
 vim.opt.termguicolors = true
 
 -- clipboard
