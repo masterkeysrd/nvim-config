3bf8e0cd83a1b1923e8ba539c6e231dc292f918c
David Morales <morales.david1997@gmail.com>
2024-06-13 12:16:43 -0400

    chore(comp): update lsp docs scroll mappings

--- after/plugin/completion.lua
+++ after/plugin/completion.lua
@@ -55,8 +55,8 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
     ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-j>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-k>'] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ["<CR>"] = confirm,
