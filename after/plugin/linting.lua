2e0d73925d1e2816a9ba4cb4a8aca56ebf417d0d
David Morales <morales.david1997@gmail.com>
2024-06-13 21:30:57 -0400

    refactor(lint): rename lint to linter

---
+++ after/plugin/linting.lua
@@ -0,0 +1,22 @@
+local ok, lint = pcall(require, 'lint')
+local notify_opts = { title = 'Linting' }
+
+if not ok then
+  vim.notify('nvim-lint not installed', vim.log.levels.ERROR, notify_opts)
+  return
+end
+
+lint.linters_by_ft = {
+  markdown = { 'markdownlint', 'vale' },
+  javascript = { 'eslint' },
+  typescript = { 'eslint' },
+  go = { 'golangcilint' },
+  proto = { 'buf' },
+}
+
+vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost', 'InsertLeave' }, {
+  group = vim.api.nvim_create_augroup('linting', { clear = true }),
+  callback = function()
+    require('lint').try_lint()
+  end,
+})
