a2beeb617f3878b1ec88f75c52e12f1ee72be919
David Morales <morales.david1997@gmail.com>
2024-06-14 19:27:03 -0400

    fix(lsp): exclude files for codelens

--- lua/masterkeysrd/lsp.lua
+++ lua/masterkeysrd/lsp.lua
@@ -2,6 +2,24 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave' }, {
   group = vim.api.nvim_create_augroup('codelens', { clear = true }),
   desc = 'Refresh codelens on BufEnter and InsertLeave',
   callback = function()
+    local exclude_ft = {
+      'fugitiveblame',
+      'fugitive',
+      'git',
+      'gitignore',
+      'gitcommit',
+      'gitrebase',
+      'gitconfig',
+      'gitstatus',
+      'gitcommit',
+      'dashboard',
+      'NvimTree',
+    }
+
+    if vim.tbl_contains(exclude_ft, vim.bo.filetype) then
+      return
+    end
+
     vim.lsp.codelens.refresh({ bufnr = 0 })
   end,
 })
