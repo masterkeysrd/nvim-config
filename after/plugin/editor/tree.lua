7823ac507d2e1327c78b373e6c407e2ca3d1473e
David Morales <morales.david1997@gmail.com>
2024-06-14 19:44:24 -0400

    feat(editor): open file after created it on nvim-tree

--- after/plugin/editor/tree.lua
+++ after/plugin/editor/tree.lua
@@ -83,6 +83,14 @@ nvim_tree.setup({
   }
 })
 
+
+
+local api = require("nvim-tree.api")
+
+api.events.subscribe(api.events.Event.FileCreated, function(file)
+  vim.cmd("edit " .. file.fname)
+end)
+
 local map = vim.keymap.set
 local function opts(desc)
   return { noremap = true, silent = true, desc = desc }
