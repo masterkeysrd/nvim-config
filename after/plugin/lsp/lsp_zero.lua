3bcde419e6ee1702edc1c9d1c0984ff007df7048
David Morales <morales.david1997@gmail.com>
2024-06-15 13:27:19 -0400

    refactor(lsp): add support for golang

--- after/plugin/lsp/lsp_zero.lua
+++ after/plugin/lsp/lsp_zero.lua
@@ -23,9 +23,128 @@ lsp_zero.on_attach(function(_, bufnr)
   keymap("n", "<leader>cr", vim.lsp.buf.rename, opts("Rename symbol"))
 end)
 
+lsp_zero.set_server_config({
+  on_init = function(client)
+    if client.name == 'gopls' and not client.server_capabilities.semanticTokensProvider then
+      local semantic = client.config.capabilities.textDocument.semanticTokens
+      client.server_capabilities.semanticTokensProvider = {
+        full = true,
+        legend = {
+          tokenTypes = semantic.tokenTypes,
+          tokenModifiers = semantic.tokenModifiers,
+        },
+        range = true,
+      }
+    end
+  end,
+})
+
 lsp_zero.set_sign_icons({
   error = "",
   warn = "",
   hint = "",
   info = "",
 })
+
+local found_mason, mason = pcall(require, "mason")
+
+if not found_mason then
+  vim.notify("mason not found", vim.log.levels.ERROR, notify_opts)
+  return
+end
+
+mason.setup({})
+
+local found_mason_lspconfig, mason_lspconfig = pcall(require, "mason-lspconfig")
+
+if not found_mason_lspconfig then
+  vim.notify("mason-lspconfig not found", vim.log.levels.ERROR, notify_opts)
+  return
+end
+
+mason_lspconfig.setup({
+  ensure_installed = {},
+  handlers = {
+    lsp_zero.default_setup,
+  }
+})
+
+local lspconfig = require("lspconfig")
+
+lspconfig.lua_ls.setup({
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT'
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+        }
+      },
+      codeLens = {
+        enable = true,
+      },
+      completion = {
+        callSnippet = 'Replace',
+      },
+      doc = {
+        privateName = { "^_" },
+      },
+      telemetry = {
+        enable = false
+      },
+      hint = {
+        enable = true,
+        setType = false,
+        paramType = true,
+        paramName = true,
+        semicolon = "Disable",
+        arrayIndex = "Disable",
+      }
+
+    }
+  }
+})
+
+lspconfig.gopls.setup({
+  settings = {
+    gopls = {
+      gofumpt = true,
+      codelenses = {
+        gc_details = false,
+        generate = true,
+        regenerate_cgo = true,
+        run_govulncheck = true,
+        test = true,
+        tidy = true,
+        upgrade_dependency = true,
+        vendor = true,
+      },
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+      analyses = {
+        fieldalignment = true,
+        nilness = true,
+        unusedparams = true,
+        unusedwrite = true,
+        useany = true,
+      },
+      usePlaceholders = true,
+      completeUnimported = true,
+      staticcheck = true,
+      directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+      semanticTokens = true,
+    }
+  }
+})
+
+vim.keymap.set("n", "<leader>cm", "<cmd>Mason<cr>", { noremap = true, silent = true, desc = "Mason" })
