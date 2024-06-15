0aa8608fbbdfe73828e53cba4e86da8512f09327
David Morales <morales.david1997@gmail.com>
2024-06-14 23:16:27 -0400

    refactor(packer): reorganize deps into groups

--- lua/masterkeysrd/packer.lua
+++ lua/masterkeysrd/packer.lua
@@ -6,54 +6,6 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use { 'wbthomason/packer.nvim' }
-  use { 'tpope/vim-fugitive' }
-  use { 'nvim-lualine/lualine.nvim' }
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    requires = {
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
-      { 'neovim/nvim-lspconfig' },
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-cmdline' },
-      { 'hrsh7th/cmp-path' },
-      { 'petertriho/cmp-git' },
-      { 'L3MON4D3/LuaSnip' },
-    }
-  }
-  use {
-    'akinsho/toggleterm.nvim',
-    config = true
-  }
-  use {
-    'akinsho/bufferline.nvim',
-    tag = "*",
-    requires = 'nvim-tree/nvim-web-devicons'
-  }
-  use { 'RRethy/vim-illuminate' }
-  use { 'lukas-reineke/indent-blankline.nvim' }
-  use { 'echasnovski/mini.indentscope' }
-  use {
-    'nvimdev/dashboard-nvim',
-    requires = { 'nvim-tree/nvim-web-devicons' }
-  }
-  use { "github/copilot.vim" }
-
-
-  use { 'echasnovski/mini.bufremove' }
-  use { 'mistricky/codesnap.nvim', run = 'make' }
-  use { 'MunifTanjim/nui.nvim' }
-  use { 'rcarriga/nvim-notify' }
-  use {
-    'folke/noice.nvim',
-    requires = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    }
-  }
 
   -- Theme Plugins
   use { 'rose-pine/neovim' }
@@ -61,6 +13,7 @@ return require('packer').startup(function(use)
   -- Coding Plugins
   use { 'echasnovski/mini.pairs' }
   use { 'folke/ts-comments.nvim' }
+  use { "github/copilot.vim" }
   use {
     'hrsh7th/nvim-cmp',
     requires = {
@@ -100,15 +53,43 @@ return require('packer').startup(function(use)
   use { "folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim" }
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.6', requires = 'nvim-lua/plenary.nvim' }
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use { 'mistricky/codesnap.nvim', run = 'make' }
+  use { 'echasnovski/mini.bufremove' }
+  use { 'akinsho/toggleterm.nvim', config = true }
+  use { 'tpope/vim-fugitive' }
+  use { 'RRethy/vim-illuminate' }
 
   -- Treesitter Plugins
   use {
     'nvim-treesitter/nvim-treesitter',
     { run = ':TSUpdate' }
   }
-  use({
+  use {
     "nvim-treesitter/nvim-treesitter-textobjects",
     requires = "nvim-treesitter/nvim-treesitter",
-  })
+  }
   use { 'windwp/nvim-ts-autotag' }
+
+  -- UI Plugins
+  use { 'nvim-lualine/lualine.nvim' }
+  use { 'MunifTanjim/nui.nvim' }
+  use { 'rcarriga/nvim-notify' }
+  use {
+    'akinsho/bufferline.nvim',
+    tag = "*",
+    requires = 'nvim-tree/nvim-web-devicons'
+  }
+  use {
+    'folke/noice.nvim',
+    requires = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    }
+  }
+  use { 'lukas-reineke/indent-blankline.nvim' }
+  use { 'echasnovski/mini.indentscope' }
+  use {
+    'nvimdev/dashboard-nvim',
+    requires = { 'nvim-tree/nvim-web-devicons' }
+  }
 end)
