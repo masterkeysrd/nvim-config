773d474f1712935eba07b0d8cf2ad6cd1a86190e
David Morales <morales.david1997@gmail.com>
2024-08-11 00:11:03 -0400

    feat(editor): improve fugitive tab management

--- after/plugin/editor/fugitive.lua
+++ after/plugin/editor/fugitive.lua
@@ -1,32 +1,40 @@
-local opened_in_other_tab = false
-
 function FugitiveToggle()
-  local buf = vim.api.nvim_get_current_buf()
-  local bufname = vim.api.nvim_buf_get_name(buf)
+  if FugitiveCloseCurrentTab() then
+    return
+  end
 
-  if string.match(bufname, "fugitive://") then
-    FugitiveClose()
-  else
-    FugitiveOpen()
+  FugitiveCloseCurrentTab()
+  FugitiveOpen()
+end
+
+function FugitiveCloseCurrentTab()
+  local tab = vim.api.nvim_get_current_tabpage()
+  local is_current, ok = pcall(vim.api.nvim_tabpage_get_var, tab, "is_fugitive")
+
+  if ok and is_current then
+    vim.api.nvim_command("tabclose")
+    return true
+  end
+
+  return false
+end
+
+function FugitiveCloseAllTabs()
+  local tab_list = vim.api.nvim_list_tabpages()
+
+  for _, tab in ipairs(tab_list) do
+    local result, ok = pcall(vim.api.nvim_tabpage_get_var, tab, "is_fugitive")
+
+    if ok and result then
+      vim.api.nvim_set_current_tabpage(tab)
+      vim.api.nvim_command("tabclose")
+    end
   end
 end
 
 function FugitiveOpen()
-  if opened_in_other_tab then
-    vim.cmd("tabclose")
-  end
-
-  vim.cmd("tab Git")
-  opened_in_other_tab = true
-end
-
-function FugitiveClose()
-  if opened_in_other_tab then
-    pcall(function()
-      vim.cmd("tabclose")
-    end)
-    opened_in_other_tab = false
-  end
+  vim.api.nvim_command("tab Git")
+  vim.api.nvim_tabpage_set_var(0, "is_fugitive", true)
 end
 
 vim.keymap.set("n", "<leader>gs", FugitiveToggle)
