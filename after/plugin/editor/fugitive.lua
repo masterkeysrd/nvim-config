de178b6272409ddf3cf05bc4400f07e07fda19db
David Morales <morales.david1997@gmail.com>
2024-08-12 12:01:44 -0400

    feat(editor): add git operation fugitive mapping

--- after/plugin/editor/fugitive.lua
+++ after/plugin/editor/fugitive.lua
@@ -37,4 +37,24 @@ function FugitiveOpen()
   vim.api.nvim_tabpage_set_var(0, "is_fugitive", true)
 end
 
-vim.keymap.set("n", "<leader>gs", FugitiveToggle)
+local function opts(desc)
+  return { noremap = true, silent = true, desc = desc }
+end
+
+local function confirm(msg, cmd)
+  return function()
+    local choice = vim.fn.confirm(msg, "&Yes\n&No", 2)
+
+    if choice == 1 then
+      vim.api.nvim_command(cmd)
+    end
+  end
+end
+
+vim.keymap.set("n", "<leader>gs", FugitiveToggle, opts("Toggle Git tab"))
+vim.keymap.set("n", "<leader>gc", "<cmd>G commit<CR>", opts("Commit changes"))
+
+vim.keymap.set("n", "<leader>gps", confirm("Push changes?", "G push"), opts("Push changes"))
+vim.keymap.set("n", "<leader>gpl", confirm("Pull changes?", "G pull"), opts("Pull changes"))
+vim.keymap.set("n", "<leader>gpL", confirm("Pull changes (no rebase)?", "G pull --no-rebase"),
+  opts("Pull changes with no rebase"))
