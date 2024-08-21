e24eef2362bb08d119bc499777d093242de8a9df
David Morales <morales.david1997@gmail.com>
2024-08-21 19:10:03 -0400

    feat(treesitter): add parameter navigation

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -48,10 +48,10 @@ ts_configs.setup({
     move = {
       enable = true,
       set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-      goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-      goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-      goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+      goto_next_start = { ["]f"] = "@function.outer", ["]a"] = "@parameter.inner", },
+      goto_next_end = { ["]F"] = "@function.outer", ["]A"] = "@parameter.inner", },
+      goto_previous_start = { ["[f"] = "@function.outer", ["[a"] = "@parameter.inner", },
+      goto_previous_end = { ["[F"] = "@function.outer", ["[A"] = "@parameter.inner", },
     },
   },
 })
