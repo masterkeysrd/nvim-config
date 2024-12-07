07fb87cdb7dc64b64efcc723d65d772f52180e54
David Morales <morales.david1997@gmail.com>
2024-12-07 00:33:05 -0400

    fix(ui/bufremove): correct closing behavior for specific filetypes

--- after/plugin/ui/bufremove.lua
+++ after/plugin/ui/bufremove.lua
@@ -1,7 +1,26 @@
 local found, bufrem = pcall(require, "mini.bufremove")
 
+local quit_ft = {
+  "qf",
+  "help",
+  "toggleterm",
+}
+
+local force_bd_ft = {
+}
+
 local function close_buffer()
   local bufnr = vim.api.nvim_get_current_buf()
+
+  if vim.tbl_contains(quit_ft, vim.bo[bufnr].filetype) then
+    vim.cmd("q")
+    return
+  end
+
+  if vim.tbl_contains(force_bd_ft, vim.bo[bufnr].filetype) then
+    vim.cmd("bd " .. bufnr)
+    return
+  end
 
   if found then
     bufrem.delete(bufnr)
